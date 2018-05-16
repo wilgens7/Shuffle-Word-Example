33173e7c33cde6e1be56c64f1d10e6ee287b3e75
Wilgens Pierre <wilgens7@gmail.com>
2018-05-16 14:45:25 -0400

    Used static method scramble instead of hard code

--- ShuffleWord.java
+++ ShuffleWord.java
@@ -7,24 +7,32 @@ import java.util.Arrays;
 
 class ShuffleWord{
 
+	private static Random randGenerator;
+
 	//swaps values at given indeces i and j. 
 	private static void swap(Comparable[] array, int i, int j){
 
 		Comparable holder=array[i];
 		array[i]=array[j];
 		array[j]=holder;
+	}
+	//scramples word
+	private static String[] scrample(String word){
 
+		String[] scrambledWord = word.split("");
+		for(int i=0; i<word.length(); i++){
+			int randomIndex=randGenerator.nextInt(i+1);
+			swap(scrambledWord,i,randomIndex);
+		}
+		return scrambledWord;
 	}
 	
 	public static void main(String[] args)throws Exception/*BAD, SHOULD USE TRY CATCH.*/{
 
-
-		
 		File file = new File("words.txt");
 		Scanner reader = new Scanner(file);
-		String[] scrambledWord;
 
-		Random randGenerator = new Random();
+		randGenerator = new Random();
 		String word=""; 
 
 		//picks a random line in the file
@@ -36,17 +44,9 @@ class ShuffleWord{
 		}
 		//prints out word, should be taken out in real program.
 		System.out.println(word);
-
-		//scramples word
-		scrambledWord = word.split("");
-		for(int i=0; i<word.length(); i++){
-			int randomIndex=randGenerator.nextInt(i+1);
-			swap(scrambledWord,i,randomIndex);
-			
-		}
 		
 		//prints array in form of a string. 
-		for(String letters:scrambledWord){
+		for(String letters:scrample(word)){
 			System.out.print(letters);
 		}
 		System.out.println("\n");
